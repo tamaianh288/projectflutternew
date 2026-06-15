bf080ea862e1778174f932680496afe03384b858
baohg <baohg@gpstech.vn>
2026-06-15 17:16:36 +0700

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,199 @@
 import 'package:flutter/material.dart';
+import 'list_message_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'App Chat',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginScreen> createState() => LoginScreenState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginScreenState extends State<LoginScreen>{
+  bool isRemembered = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/images/brgLogin.jpg'), fit: BoxFit.cover),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+            child: Center(
+              child:
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50,),
+                  Image(
+                    image: AssetImage('assets/images/ImageLogin.png'),
+                    height: 170,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    // height: MediaQuery.of(context).size.width*0.95,
+                    width: MediaQuery.of(context).size.width*0.85,
+                    decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                    padding: EdgeInsets.all(25.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Email',
+                          style: TextStyle(color: Colors.red[800], fontWeight: FontWeight.bold, fontSize: 17.0,),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: WidgetStateColor.resolveWith((states){
+                              if(states.contains(WidgetState.focused)){
+                                return Colors.red[50]!;
+                              }
+                              return Colors.white;
+                            }),
+                              enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.red, width: 1.0),),
+                              focusedBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red, width: 2.0), // Có thể cho viền dày hơn chút khi focus
+                              ),
+                              hintText: 'trung@gmail.com',
+                              prefixIcon: Icon(Icons.email, color: Colors.red[400],),
+                          ),
+                          style: TextStyle(color: Colors.red[800]),
+                          //Đổi màu con trỏ chuột nhấp nháy thành màu đỏ
+                          cursorColor: Colors.red[800],
+                        ),
+                        SizedBox(height: 20,),
+                        Text(
+                          'Mật khẩu',
+                          style: TextStyle(color: Colors.red[800], fontWeight: FontWeight.bold, fontSize: 17.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: WidgetStateColor.resolveWith((states){
+                              if(states.contains(WidgetState.focused)){
+                                return Colors.red[50]!;
+                              }
+                              return Colors.white;
+                            }),
+                            enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.red, width: 1.0),),
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red, width: 2.0), // Có thể cho viền dày hơn chút khi focus
+                            ),
+                            hintText: '******',
+                            prefixIcon: Icon(Icons.lock, color: Colors.red[400],),
+                            suffixIcon: Icon(Icons.visibility_off, color: Colors.red[400],)
+                          ),
+                          style: TextStyle(color: Colors.red[800]),
+                          //Đổi màu con trỏ chuột nhấp nháy thành màu đỏ
+                          cursorColor: Colors.red[800],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Checkbox(
+                                value: isRemembered,
+                                activeColor: const Color(0xFFEF5350),
+                                side: const BorderSide(color: Color(0xFFEF5350),width: 1.0),
+                                onChanged: (bool? newValue){
+                                  setState(() {
+                                    isRemembered = newValue ?? false;
+                                  });
+                                },
+                            ),
+                            Text('Nhớ tài khoản', style: TextStyle(color: Colors.red[800])),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: (){
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => ListMessageScreen(),));
+                              },
+                              child: Text('Đăng nhập', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red[400],
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                            ),
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Expanded(child: Divider(
+                              color: Colors.red[400],
+                              thickness: 1,
+                            ),),
+                            Container(
+                              margin: EdgeInsets.all(5),
+                              child: Text('hoặc đăng nhập với', style: TextStyle(color: Colors.red[400], fontSize: 12.0),),
+                            ),
+                            Expanded(child: Divider(
+                              color: Colors.red[400],
+                              thickness: 1,
+                            ),),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [ OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(color: Color(0xFFEF5350), width: 1.5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                              onPressed: (){},
+                              child: Text('Khách', style: TextStyle(color: Colors.red[400]),)
+                          ),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text.rich(TextSpan(
+                              text: 'Chưa có tài khoản? ',
+                              children: <TextSpan>[
+                                TextSpan(text: ' Đăng ký', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red[400]))
+                              ],
+                            )),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
